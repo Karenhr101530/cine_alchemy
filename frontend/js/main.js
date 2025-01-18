bd78f87eebb98c64845233e1d27a27441ea6ba36
karenhr101530 <karenhr101530@gmail.com>
2025-01-18 09:24:45 -0500

    Design Responsive

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -40,3 +40,12 @@ function createFloatingMovies() {
 }
 
 createFloatingMovies();
+
+
+  const hamburger = document.querySelector('.hamburger');
+  const navButtons = document.querySelector('.nav-buttons');
+
+  hamburger.addEventListener('click', () => {
+    navButtons.classList.toggle('active');
+  });
+
